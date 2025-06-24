651b017292cb55c01b008d919cd5a42cc6ec58b3
amansharma2380 <adipandit2380@gmail.com>
2025-06-24 16:22:06 +0530

    commit

--- projects/JavaScript/file16.js
+++ projects/JavaScript/file16.js
@@ -26,8 +26,26 @@
 //     .then((n)=>f2(n))
 //     .catch((err) => console.log(err));
 
+//fetching api
 
-fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res)=>res.json())
-    .then((data)=> {})
-    .catch((err) => console.log(err));
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((res)=>res.json())
+//     .then((data)=> {
+//         data.map(value=>{
+//             console.log(value.name, value.email);
+//         })
+//     })
+//     .catch((err) => console.log(err));
+
+
+
+const fetchData =  async () =>{
+    const res =  await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+
+     data.map(value=>{
+            console.log(value.name, value.email);
+        })
+};
+
+fetchData();
